72fd7cc48cc7731198545e81a6047545086467a5
Dannylew12 <dannylew12@yahooo.com>
2023-02-09 13:55:15 -0800

    Added alignment for channel image in channelcard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,35 +1,45 @@
-import {Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
 import { channelDetail } from './ChannelDetail';
 
-const ChannelCard = ({channelDetail, marginTop}) => (
+const ChannelCard = ({ channelDetail, marginTop }) => (
 	<Box sx={{
-		boxShadow: 'none', 
+		boxShadow: 'none',
 		borderRadius: '20px',
 		display: 'flex',
 		justifyContent: 'center',
 		alignItems: 'center',
-		width: { xs: '356px', md: '320px'},
+		width: { xs: '356px', md: '320px' },
 		height: '326px',
 		margin: 'auto',
 		marginTop
 	}}>
 		<Link to={`/channel/${channelDetail?.id?.channelId}`}>
-			<CardContent sx={{ display: 'flex', flexDirection: 'column',
-			justifyContent: 'center', textAlign: 'center', color: '#fff'}} >
+			<CardContent sx={{
+				display: 'flex', flexDirection: 'column',
+				justifyContent: 'center', textAlign: 'center', color: '#fff', alignItems: 'center'
+			}} >
 				<CardMedia
 					image={channelDetail?.snippet?.thumbnails?.high?.url ||
-					demoProfilePicture}
+						demoProfilePicture}
 					alt={channelDetail?.snippet?.title}
-					sx={{ borderRadius: '50%', height: '180px',
-					width: '180px', mb: 2, border: '1px solid #e3e3e3'}}
+					sx={{
+						borderRadius: '50%',
+						height: '180px',
+						width: '180px',
+						mb: 2,
+						border: '1px solid #e3e3e3',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
 				/>
 				<Typography variant='h6'>
 					{channelDetail?.snippet?.title}
-					<CheckCircle sx={{ fontSize: 16, color: 'gray', ml: '5px'}} />
+					<CheckCircle sx={{ fontSize: 16, color: 'gray', ml: '5px' }} />
 				</Typography>
 				{channelDetail?.statistics?.subscriberCount && (
 					<Typography>
